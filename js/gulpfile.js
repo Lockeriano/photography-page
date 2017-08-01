8c5c617c2cee566c49d15ad2d78619c84b5c4100
Lockeriano <patryk.skrzyniarz19@gmail.com>
2017-08-01 12:19:27 +0200

    HTML markup done

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -5,10 +5,10 @@ var sass = require('gulp-sass');
 gulp.task('sass', function() {
   return gulp.src('../css/**/*.sass') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass())
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('../css'))
 })
 
 gulp.task('watch', function(){
-  gulp.watch('../css/**/*.sass', ['sass']); 
+  gulp.watch('../css/**/*.sass', ['sass']);
   // Other watchers
 })
